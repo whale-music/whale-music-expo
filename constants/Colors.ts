fc8b026659e181355f935721c26d13d3c957846d
asddfdf <468382010@qq.com>
2024-04-08 16:42:33 +0800

    feat: add theme color

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -5,6 +5,7 @@ export default {
   light: {
     text: '#000',
     background: '#fff',
+    secondaryBackground: '#f2f2f2',
     tint: tintColorLight,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
@@ -12,6 +13,7 @@ export default {
   dark: {
     text: '#fff',
     background: '#000',
+    secondaryBackground: '#333333',
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
