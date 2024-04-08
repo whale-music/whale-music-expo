6904bbc58e12498092c2a259a5a89ce948ae746d
asddfdf <468382010@qq.com>
2024-04-08 16:42:56 +0800

    feat: add resource VirtualizedList

--- app/(tabs)/library.tsx
+++ app/(tabs)/library.tsx
@@ -1,14 +1,20 @@
 import { SafeAreaView, StyleSheet, TouchableOpacity, VirtualizedList } from 'react-native';
-import { NativeText, NativeView } from '@/components/Themed';
-import { useEffect, useState } from 'react'
+import { NativeButton, NativeText, NativeView } from '@/components/Themed';
+import { useEffect, useRef, useState } from 'react'
 import { getResource, Resource } from '@/api/resource'
 import { AudioLines } from 'lucide-react-native'
 import { useColorScheme } from '@/components/useColorScheme';
 import { Theme } from '@/constants/Theme'
+import ActionSheet, { ActionSheetRef } from 'react-native-actions-sheet'
 
 export default function App() {
+    const theme = Theme()
 
+    const selectItem = useRef<Resource>();
+    // const [ selectItem, setSelectItem ] = useState<Resource>()
     const [ resources, setResources ] = useState<Resource[]>([])
+
+    const actionSheetRef = useRef<ActionSheetRef>(null);
 
     useEffect(() => {
         (async function () {
@@ -29,42 +35,92 @@ export default function App() {
         )
     }
 
+    function Item({value}: { value: Resource }) {
+        const light = (useColorScheme() ?? 'light') === 'light';
+        const iconColor = light ? '#000' : '#fff'
+        const theme = Theme()
+
+        const resource = value
+        const onPress = () => {
+            selectItem.current = resource
+            actionSheetRef.current?.show();
+        }
+        return (
+            <>
+                <TouchableOpacity
+                    style={ {backgroundColor: theme.background} }
+                    onPress={ onPress }>
+                    <NativeView style={ [ {opacity: resource.status ? 1 : 0.5}, styles.item ] }>
+                        <AudioLines size={ 28 } color={ iconColor }/>
+                        <NativeView style={ styles.itemTitle }>
+                            <NativeText numberOfLines={ 1 }>
+                                { resource.name }
+                            </NativeText>
+                            <NativeText numberOfLines={ 1 } style={ {opacity: 0.4} }>
+                                { resource.path }
+                            </NativeText>
+                        </NativeView>
+                    </NativeView>
+                </TouchableOpacity>
+            </>
+        )
+    }
+
+    function ActionSheetContent() {
+        const current = selectItem.current
+        const bgStyle = {backgroundColor: theme.secondaryBackground};
+        if (!current) {
+            return (
+                <NativeView style={ [ {flex: 1, justifyContent: 'center', alignItems: 'center',}, bgStyle ] }>
+                    <NativeText style={ {fontSize: 20} }>请重新选择数据</NativeText>
+                </NativeView>
+            )
+        } else {
+            return (
+                <NativeView style={ [ {paddingTop: 20, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}, bgStyle ] }>
+                    <NativeText style={ {fontSize: 15} }>{ current?.name }</NativeText>
+                    <NativeButton style={ {backgroundColor: 'white', borderWidth: 1} } textStyle={ {color: 'black'} }>播放</NativeButton>
+                </NativeView>
+            )
+        }
+    }
+
+    const NativeActionSheet = () => (
+        <ActionSheet ref={ actionSheetRef }
+                     containerStyle={ {
+                         borderTopLeftRadius: 25,
+                         borderTopRightRadius: 25,
+                         backgroundColor: theme.secondaryBackground
+                     } }
+                     indicatorStyle={ {
+                         width: 100,
+                         opacity: 0.6,
+                         backgroundColor: theme.text
+                     } }
+                     gestureEnabled={ true }
+        >
+            <NativeView style={ {height: 200, backgroundColor: theme.secondaryBackground} }>
+                <ActionSheetContent/>
+            </NativeView>
+        </ActionSheet>
+    )
     return (
-        <SafeAreaView>
-            <VirtualizedList
-                initialNumToRender={ 4 }
-                renderItem={ ({item}) => <Item resources={ item }/> }
-                keyExtractor={ item => item.name }
-                getItemCount={ () => resources.length }
-                getItem={ getItem }
-                ItemSeparatorComponent={ ItemSeparator }
-            />
-        </SafeAreaView>
+        <>
+            <NativeActionSheet/>
+            <SafeAreaView>
+                <VirtualizedList
+                    initialNumToRender={ 20 }
+                    renderItem={ ({item}) => <Item value={ item }/> }
+                    keyExtractor={ item => item.path }
+                    getItemCount={ () => resources.length }
+                    getItem={ getItem }
+                    ItemSeparatorComponent={ ItemSeparator }
+                />
+            </SafeAreaView>
+        </>
     );
 }
 
-function Item({resources}: { resources: Resource }) {
-    const light = (useColorScheme() ?? 'light') === 'light';
-    const iconColor = light ? '#000' : '#fff'
-    const theme = Theme()
-    return (
-        <>
-            <TouchableOpacity style={ {backgroundColor: theme.background} }>
-                <NativeView style={ [ {opacity: resources.status ? 1 : 0.5}, styles.item ] }>
-                    <AudioLines size={ 28 } color={ iconColor }/>
-                    <NativeView style={ styles.itemTitle }>
-                        <NativeText numberOfLines={ 1 }>
-                            { resources.name }
-                        </NativeText>
-                        <NativeText numberOfLines={ 1 } style={ {opacity: 0.4} }>
-                            { resources.path }
-                        </NativeText>
-                    </NativeView>
-                </NativeView>
-            </TouchableOpacity>
-        </>
-    )
-}
 
 const ItemSeparator = () => (
     <NativeView style={ {flexDirection: "row-reverse"} }>
