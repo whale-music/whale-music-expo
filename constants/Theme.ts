62e792f8f581e665d8bd3bc1d29ea43b14478f4a
asddfdf <468382010@qq.com>
2024-04-08 22:18:39 +0800

    feat: add theme mode method

--- constants/Theme.ts
+++ constants/Theme.ts
@@ -2,7 +2,19 @@ import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme'
 
 type ColorType = typeof Colors.light & typeof Colors.dark;
+
+
 export const Theme = (): ColorType => {
     const colorScheme = useColorScheme();
     return Colors[colorScheme ?? 'light'];
+}
+
+/**
+ * true:  light
+ * false: dark
+ * @constructor
+ */
+export const ThemeMode = (): boolean => {
+    const colorScheme = useColorScheme();
+    return (colorScheme ?? 'light') === 'light'
 }
