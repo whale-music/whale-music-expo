df52b2e16f039fdc00bb0cc65be6de700e3f99dd
asddfdf <468382010@qq.com>
2024-04-09 09:45:24 +0800

    perf: login error

--- util/http/request.ts
+++ util/http/request.ts
@@ -1,36 +1,59 @@
-import { getServerUrlSettingStoreData } from '@/store/setting'
-import { getUserToken } from '@/store/user'
-import Toast from 'react-native-root-toast'
+import { getServerUrlSettingStoreData } from "@/store/setting";
+import { getUserToken } from "@/store/user";
+import Toast from "react-native-root-toast";
+import { router } from "expo-router";
 
-export const request = async <T>(method: "get" | "post", url: string, body?: BodyInit, headers?: HeadersInit, timeout: number = 10_000, prop = {}): Promise<T> => {
-    const baseurl = await getServerUrlSettingStoreData()
-    url = baseurl + url;
+export const request = async <T>(
+  method: "get" | "post",
+  url: string,
+  body?: BodyInit,
+  headers?: HeadersInit,
+  timeout: number = 10_000,
+  prop = {},
+): Promise<T> => {
+  const baseurl = await getServerUrlSettingStoreData();
+  url = baseurl + url;
 
-    let token = await getUserToken()
-    const _headers = {
-        'Content-Type': 'application/json',
-        "Authorization": `Bearer ${ token }`,
-        ...headers
-    }
-    console.log(url, 'url')
-    console.log(token, 'token')
+  let token = await getUserToken();
+  const _headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+    ...headers,
+  };
+  console.log(url, "url");
+  console.log(token, "token");
 
-    let controller = new AbortController()
-    let id = setTimeout(() => controller.abort(), timeout)
-    let r: Response = await fetch(url, {method: method, headers: _headers, body, signal: controller.signal, ...prop})
-    clearTimeout(id)
-    if (r.status != 200) {
-        Toast.show(`http status error code ${ r.status }`)
-        throw new Error(`request error status ${ r.status }`)
+  let controller = new AbortController();
+  let id = setTimeout(() => controller.abort(), timeout);
+  let r: Response = await fetch(url, {
+    method: method,
+    headers: _headers,
+    body,
+    signal: controller.signal,
+    ...prop,
+  });
+  clearTimeout(id);
+  if (r.status != 200) {
+    Toast.show(`http status error code ${r.status}`, {
+      position: Toast.positions.CENTER,
+    });
+    throw new Error(`request error status ${r.status}`);
+  }
+  const json = await r.json();
+  if (json?.code != 200) {
+    if (json?.code == 20001) {
+      router.replace("/login");
+      Toast.show("please login");
+      throw new Error("Token expired");
     }
-    const json = await r.json();
-    if (json?.code != 200) {
-        const s = `service error code ${ json?.code }, msg: ${ json?.message }`
-        Toast.show(s)
-        throw new Error(s);
-    }
-    return json["data"]
-}
+    const s = `service error code ${json?.code}, msg: ${json?.message}`;
+    Toast.show(s, {
+      position: Toast.positions.CENTER,
+    });
+    throw new Error(s);
+  }
+  return json["data"];
+};
 
 export const get = <T>(url: string, header?: HeadersInit, timeout?: number, prop = {}): Promise<T> => {
     return request('get', url, undefined, header, timeout, prop)
