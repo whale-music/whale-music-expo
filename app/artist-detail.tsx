f26d02038f5ade230324042df7fe3a3b7fafbe0d
asddfdf <468382010@qq.com>
2024-04-11 13:47:15 +0800

    perf: artist detail play button

--- app/artist-detail.tsx
+++ app/artist-detail.tsx
@@ -41,17 +41,15 @@ export default function ArtistDetail() {
                         <NativeText style={{ fontSize: 20, fontWeight: "700" }}>{artistDetail?.artistName}</NativeText>
                         <NativeText style={{ fontSize: 15, fontWeight: "400", opacity: 0.6 }}>{artistDetail?.aliasName}</NativeText>
                         <NativeView style={{ flexDirection: "row", gap: 10, alignItems: "center" }}>
-                            <NativeButton>
-                                <NativeView style={{ flexDirection: "row", alignItems: "center" }}>
-                                    <Play fill={theme.accentForeground} color={theme.accentForeground} size={20} />
-                                    <NativeText>Play</NativeText>
-                                </NativeView>
+                            <NativeButton
+                                style={{ backgroundColor: "white" }}
+                                textStyle={{ color: "#000" }}
+                                icon={<Play fill="#000" color="#000" size={20} />}
+                            >
+                                Play
                             </NativeButton>
-                            <NativeButton>
-                                <NativeView style={{ flexDirection: "row", alignItems: "center", backgroundColor: theme.card }}>
-                                    <ListPlus size={20} color={theme.accentForeground} />
-                                    <NativeText style={{ fontWeight: "600" }}>收藏</NativeText>
-                                </NativeView>
+                            <NativeButton style={{ borderColor: theme.accent }} icon={<ListPlus size={20} color={theme.accentForeground} />}>
+                                收藏
                             </NativeButton>
                         </NativeView>
                     </NativeView>
