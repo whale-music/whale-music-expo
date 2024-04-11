f1c8ca369618fa1e764dc7ea342b4fe61a6912d1
asddfdf <468382010@qq.com>
2024-04-11 11:54:25 +0800

    fix: ios height error

--- app/music-detail.tsx
+++ app/music-detail.tsx
@@ -33,80 +33,71 @@ export default function musicDetail() {
     }, []);
 
     return (
-        <>
-            <NativeView style={styles.container}>
-                <SafeAreaView>
-                    <ScrollView>
-                        <NativeView style={{ flexDirection: "column", alignItems: "center", gap: 20 }}>
-                            <Image source={{ uri: musicInfo?.picUrl }} style={{ width: 250, aspectRatio: 1, borderRadius: 20 }} />
-                            <NativeView
-                                style={{ backgroundColor: "transparent", flexDirection: "column", alignItems: "center", paddingHorizontal: 20 }}
-                            >
-                                <NativeText style={{ fontSize: 20, fontWeight: "700" }}>{musicInfo?.musicName}</NativeText>
-                                <NativeText style={{ fontSize: 15, fontWeight: "400", opacity: 0.6 }}>{musicInfo?.aliasName}</NativeText>
-                                <NativeView style={{ flexDirection: "row", gap: 10, alignItems: "center" }}>
-                                    <NativeButton>
-                                        <NativeView style={{ flexDirection: "row", alignItems: "center" }}>
-                                            <Play fill={theme.accentForeground} color={theme.accentForeground} size={20} />
-                                            <NativeText>Play</NativeText>
-                                        </NativeView>
-                                    </NativeButton>
-                                    <NativeButton>
-                                        <NativeView style={{ flexDirection: "row", alignItems: "center" }}>
-                                            <ListPlus size={20} color={theme.accentForeground} />
-                                            <NativeText style={{ fontWeight: "600" }}>收藏</NativeText>
-                                        </NativeView>
-                                    </NativeButton>
-                                    {/*<Heart size={28} color={theme.accentForeground} />*/}
+        <SafeAreaView style={styles.container}>
+            <ScrollView>
+                <NativeView style={{ flexDirection: "column", alignItems: "center", gap: 20 }}>
+                    <Image source={{ uri: musicInfo?.picUrl }} style={{ width: 250, aspectRatio: 1, borderRadius: 20 }} />
+                    <NativeView style={{ backgroundColor: "transparent", flexDirection: "column", alignItems: "center", paddingHorizontal: 20 }}>
+                        <NativeText style={{ fontSize: 20, fontWeight: "700" }}>{musicInfo?.musicName}</NativeText>
+                        <NativeText style={{ fontSize: 15, fontWeight: "400", opacity: 0.6 }}>{musicInfo?.aliasName}</NativeText>
+                        <NativeView style={{ flexDirection: "row", gap: 10, alignItems: "center" }}>
+                            <NativeButton>
+                                <NativeView style={{ flexDirection: "row", alignItems: "center" }}>
+                                    <Play fill={theme.accentForeground} color={theme.accentForeground} size={20} />
+                                    <NativeText>Play</NativeText>
                                 </NativeView>
-                            </NativeView>
+                            </NativeButton>
+                            <NativeButton>
+                                <NativeView style={{ flexDirection: "row", alignItems: "center" }}>
+                                    <ListPlus size={20} color={theme.accentForeground} />
+                                    <NativeText style={{ fontWeight: "600" }}>收藏</NativeText>
+                                </NativeView>
+                            </NativeButton>
+                            {/*<Heart size={28} color={theme.accentForeground} />*/}
                         </NativeView>
-                        <NativeView style={{ marginHorizontal: 10 }}>
-                            <NativeText style={{ fontSize: 40, fontWeight: "600" }}>音源</NativeText>
-                            <NativeView style={{ flexDirection: "column", gap: 10 }}>
-                                {musicInfo?.sources.map((v) => {
-                                    return (
-                                        <>
-                                            <NativeView
-                                                key={v.id}
-                                                style={{
-                                                    backgroundColor: theme.card,
-                                                    borderWidth: 1,
-                                                    borderColor: theme.cardForeground,
-                                                    borderRadius: 10,
-                                                    height: 50,
-                                                    flexDirection: "row",
-                                                    alignItems: "center",
-                                                    justifyContent: "space-between",
-                                                    paddingHorizontal: 10,
-                                                }}
-                                            >
-                                                <NativeText style={{ fontSize: 12, fontWeight: "500", opacity: 0.5 }}>{v.md5}</NativeText>
-                                                <NativeView
-                                                    style={{
-                                                        backgroundColor:
-                                                            v.path === null || v.path === undefined || v.path === "" ? "gray" : "#38b48b",
-                                                        width: 10,
-                                                        height: 10,
-                                                        borderRadius: 9999,
-                                                    }}
-                                                />
-                                            </NativeView>
-                                        </>
-                                    );
-                                })}
-                            </NativeView>
-                        </NativeView>
-                    </ScrollView>
-                </SafeAreaView>
-            </NativeView>
-        </>
+                    </NativeView>
+                </NativeView>
+                <NativeView style={{ marginHorizontal: 10 }}>
+                    <NativeText style={{ fontSize: 40, fontWeight: "600" }}>音源</NativeText>
+                    <NativeView style={{ flexDirection: "column", gap: 10 }}>
+                        {musicInfo?.sources.map((v) => {
+                            return (
+                                <NativeView
+                                    key={v.id}
+                                    style={{
+                                        backgroundColor: theme.card,
+                                        borderWidth: 1,
+                                        borderColor: theme.cardForeground,
+                                        borderRadius: 10,
+                                        height: 50,
+                                        flexDirection: "row",
+                                        alignItems: "center",
+                                        justifyContent: "space-between",
+                                        paddingHorizontal: 10,
+                                    }}
+                                >
+                                    <NativeText style={{ fontSize: 12, fontWeight: "500", opacity: 0.5 }}>{v.md5}</NativeText>
+                                    <NativeView
+                                        style={{
+                                            backgroundColor: v.path === null || v.path === undefined || v.path === "" ? "gray" : "#38b48b",
+                                            width: 10,
+                                            height: 10,
+                                            borderRadius: 9999,
+                                        }}
+                                    />
+                                </NativeView>
+                            );
+                        })}
+                    </NativeView>
+                </NativeView>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        // flex: 1,
+        flex: 1,
         // justifyContent: "center",
         // alignItems: "center",
         // marginTop: StatusBar.currentHeight || 0,
