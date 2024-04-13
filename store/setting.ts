689c91046d4778f7072699f7fbae0b6f4e3e82d8
asddfdf <468382010@qq.com>
2024-04-13 17:31:02 +0800

    fix: login url join

--- store/setting.ts
+++ store/setting.ts
@@ -1,11 +1,11 @@
-import { getStoreData, mergeStoreData, storeData } from '@/store/store';
-import { router } from 'expo-router';
+import { getStoreData, mergeStoreData, storeData } from "@/store/store";
+import { router } from "expo-router";
 
 export interface SettingStoreData {
     serverUrl?: string;
 }
 
-export const SETTING_KEY = 'setting-store-key';
+export const SETTING_KEY = "setting-store-key";
 
 export const getSettingStoreData = () => {
     return getStoreData<SettingStoreData>(SETTING_KEY);
@@ -15,18 +15,27 @@ export const saveSettingStoreData = async (value: SettingStoreData) => {
     await storeData(SETTING_KEY, value);
 };
 
-export const getServerUrlSettingStoreData = async (
-    isRouterJump: boolean = true
-): Promise<string | undefined> => {
+export const getServerUrlSettingStoreData = async (isRouterJump: boolean = true): Promise<string | undefined> => {
     const serverUrl = (await getStoreData<SettingStoreData>(SETTING_KEY))?.serverUrl;
     // 是否跳转， 如果没有地址则跳转到填写地址界面
     if (!serverUrl && isRouterJump) {
-        router.replace('/login');
-        throw new Error('url is null, Please enter Url');
+        router.replace("/login");
+        throw new Error("url is null, Please enter Url");
     } else {
-        return serverUrl;
+        return removeTrailingSlash(serverUrl);
     }
 };
+
+function removeTrailingSlash(str: string | undefined) {
+    if (!str) {
+        return undefined;
+    }
+    if (str.endsWith("/")) {
+        return str.slice(0, -1); // 删除尾部的 /
+    } else {
+        return str; // 不做处理
+    }
+}
 
 export const saveServerUrlSettingStoreData = async (url: string) => {
     await mergeStoreData<SettingStoreData>(SETTING_KEY, { serverUrl: url });
